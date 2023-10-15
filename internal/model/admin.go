28947ba20bf388699f3c7f55bc25e87bc05e82a6
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-10-15 20:43:50 +0800

    Update admin.go

--- internal/model/admin.go
+++ internal/model/admin.go
@@ -7,7 +7,7 @@ type Admin struct {
 	LoginName string    `xorm:"not null unique VARCHAR(30) login_name"`
 	NickName  string    `xorm:"null VARCHAR(30) nick_name"`
 	PasswdMd5 string    `xorm:"not null VARCHAR(255) passwdmd5"`
-	Email     string    `xorm:"null unique VARCHAR(100) email"`
+	Email     string    `xorm:"not null unique VARCHAR(100) email"`
 	Locked    int       `xorm:"not null default 0 TINYINT locked"`
 	CreatedAt time.Time `xorm:"created TIMESTAMP created_at"`
 	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at"`
