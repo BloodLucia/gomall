2178bd0ab2d3df5fb6322f6c9405d76bcd81a3f3
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-10-15 12:36:14 +0000

    添加字段到Admin模型

--- internal/model/admin.go
+++ internal/model/admin.go
@@ -8,6 +8,7 @@ type Admin struct {
 	NickName  string    `xorm:"null VARCHAR(30) nick_name"`
 	PasswdMd5 string    `xorm:"not null VARCHAR(255) passwdmd5"`
 	Email     string    `xorm:"null unique VARCHAR(100) email"`
+	Locked    int       `xorm:"not null default 0 TINYINT locked"`
 	CreatedAt time.Time `xorm:"created TIMESTAMP created_at"`
 	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at"`
 	DeletedAt time.Time `xorm:"deleted TIMESTAMP deleted_at"`
