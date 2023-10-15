aba38b7360617e0b494c6c961909393da6fd138b
Kalougata <111719756+kalougata@users.noreply.github.com>
2023-10-15 11:11:55 +0000

    [admin]注册PingController到路由

--- api/v1/admin/guest.go
+++ api/v1/admin/guest.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 // RegisterGuestAPIRouter 注册不需要登录的路由
 func (api *AdminAPIRouter) RegisterGuestAPIRouter(r *gin.RouterGroup) {
+	r.GET("/ping", api.PingCtrl.Ping)
 	r.POST("/login", api.UserCtrl.Login)
 	r.POST("/register", api.UserCtrl.Register)
 }
