75d876accdff8ad5c468a1cc865ee47058e6fb21
Kalougata <thecvcoder@foxmail.com>
2023-10-17 16:24:50 +0800

    查询用户操作应该在service层

--- internal/repo/admin/user_repo.go
+++ internal/repo/admin/user_repo.go
@@ -2,8 +2,6 @@ package adminrepo
 
 import (
 	"context"
-	"errors"
-	"log"
 
 	"github.com/kalougata/gomall/internal/data"
 	adminmodel "github.com/kalougata/gomall/internal/model/admin"
@@ -29,18 +27,8 @@ func (repo *userRepo) FindByLoginName(ctx context.Context, loginName string) (re
 
 // Create implements UserRepo.
 func (repo *userRepo) Create(ctx context.Context, model *adminmodel.User) error {
-	_, has, err := repo.FindByLoginName(ctx, model.LoginName)
-	if err != nil {
-		// TODO
-		log.Panicf("查询数据库出错: %s", err)
-	}
-	if has {
-		return errors.New("登录名已被使用, 请重新输入")
-	}
-
 	if _, err := repo.DB.Context(ctx).Insert(model); err != nil {
-		// TODO
-		log.Panicf("插入数据库出错: %s", err)
+		return err
 	}
 
 	return nil
--- internal/service/admin/user_service.go
+++ internal/service/admin/user_service.go
@@ -2,6 +2,8 @@ package adminsrv
 
 import (
 	"context"
+	"errors"
+	"log"
 
 	adminmodel "github.com/kalougata/gomall/internal/model/admin"
 	adminrepo "github.com/kalougata/gomall/internal/repo/admin"
@@ -23,11 +25,22 @@ func (srv *userService) Login(ctx context.Context) error {
 
 // Register implements UserService.
 func (srv *userService) Register(ctx context.Context, req *adminmodel.UserRegisterRequest) error {
+	_, has, err := srv.repo.FindByLoginName(ctx, req.LoginName)
+	if has {
+		return errors.New("用户名已被注册, 请重新输入")
+	}
+	if err != nil {
+		log.Panicf("查询数据库出错: %s", err)
+	}
 	model := &adminmodel.User{
 		LoginName: req.LoginName,
 		PasswdMd5: req.Passwd,
 	}
-	return srv.repo.Create(ctx, model)
+	if err = srv.repo.Create(ctx, model); err != nil {
+		log.Panicf("插入数据库出错: %s", err)
+	}
+
+	return nil
 }
 
 func NewUserService(repo adminrepo.UserRepo) UserService {
