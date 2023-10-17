c3baa5be7987fae7eb400439464712382ce8918c
Kalougata <thecvcoder@foxmail.com>
2023-10-17 15:33:18 +0800

    删除管理员用户模型的Email字段, 并声明了一个UserRegisterRequest结构体

--- internal/model/admin/user.go
+++ internal/model/admin/user.go
@@ -7,11 +7,16 @@ type User struct {
 	LoginName string    `xorm:"not null unique VARCHAR(50) login_name"`
 	NickName  string    `xorm:"null VARCHAR(50) nick_name"`
 	PasswdMd5 string    `xorm:"not null VARCHAR(255) passwdmd5"`
-	Email     string    `xorm:"not null unique VARCHAR(100) email"`
+	Email     string    `xorm:"null unique VARCHAR(100) email"`
 	Locked    int       `xorm:"not null default 0 TINYINT locked"`
 	CreatedAt time.Time `xorm:"created TIMESTAMP created_at"`
 	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at"`
 	DeletedAt time.Time `xorm:"deleted TIMESTAMP deleted_at"`
+}
+
+type UserRegisterRequest struct {
+	LoginName string `json:"login_name"`
+	Passwd    string `json:"passwd"`
 }
 
 func (u User) TableName() string {
