a483e2b70c3b2c7db276582b9241409eb4293bfb
sunset2002 <146757937+sunset2002@users.noreply.github.com>
2023-10-19 00:15:21 +0000

    创建product模型

---
+++ internal/model/admin/product.go
@@ -0,0 +1,20 @@
+package adminmodel
+
+import "time"
+
+type Product struct {
+	ID             int       `xorm:"not null pk autoincr BIGINT(20) id"`
+	CreatedAt      time.Time `xorm:"created TIMESTAMP created_at"`
+	UpdatedAt      time.Time `xorm:"updated TIMESTAMP updated_at"`
+	DeletedAt      time.Time `xorm:"deleted TIMESTAMP deleted_at"`
+	ProdName       string    `xorm:"not null VARCHAR(255) prod_name"`
+	ProdIntro      string    `xorm:"not null prod_intro"`
+	ProdCategoryId int       `xorm:"not null BIGINT(20) prod_category_id"`
+	ProdPrice      int       `xorm:"not null DECIMAL(2) prod_price"`
+	ProdStock      int       `xorm:"not null prod_stock"`
+	ProdCreator    int       `xorm:"not null BIGINT(20) prod_creator"`
+}
+
+func (Product) TableName() string {
+	return "t_mall_products"
+}
