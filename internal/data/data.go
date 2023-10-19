926730329461b4d2245e2312400bc8c169d708f4
sunset2002 <146757937+sunset2002@users.noreply.github.com>
2023-10-19 08:43:59 +0000

    成功连接到redis

--- internal/data/data.go
+++ internal/data/data.go
@@ -7,11 +7,13 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/kalougata/gomall/pkg/config"
+	"github.com/redis/go-redis/v9"
 	"xorm.io/xorm"
 )
 
 type Data struct {
-	DB *xorm.Engine
+	DB  *xorm.Engine
+	RDB *redis.Client
 }
 
 func NewData(conf *config.Config) (*Data, func(), error) {
@@ -32,13 +34,28 @@ func NewData(conf *config.Config) (*Data, func(), error) {
 		return nil, nil, err
 	}
 
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", conf.RDB.Host, conf.RDB.Port),
+		Username: conf.RDB.User,
+		Password: conf.RDB.Passwd,
+		DB:       conf.RDB.DB,
+	})
+
+	if err := rdb.Ping(context.Background()); err != nil {
+		return nil, nil, err.Err()
+	}
+
 	data := &Data{
-		DB: db,
+		DB:  db,
+		RDB: rdb,
 	}
 
 	return data, func() {
 		if err := db.Close(); err != nil {
 			log.Fatalf("falied to close database: %s", err)
+		}
+		if err := rdb.Close(); err != nil {
+			log.Fatalf("falied to close redis client: %s", err)
 		}
 	}, nil
 }
